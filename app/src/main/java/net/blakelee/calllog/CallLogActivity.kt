6581a072cbebce76dd42627b9476be558f0b7e19
Blake Oliveira <blake@blakelee.net>
2019-05-10 22:39:03 -0700

    Enable permission handling

--- app/src/main/java/net/blakelee/calllog/CallLogActivity.kt
+++ app/src/main/java/net/blakelee/calllog/CallLogActivity.kt
@@ -5,15 +5,20 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main.*
+import android.net.Uri.fromParts
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.content.Intent
 
 private const val PERMISSIONS = 100
+private const val REQUEST_PERMISSION_SETTING = 101
 
 class CallLogActivity : AppCompatActivity() {
+
+    private val permissions = arrayOf(Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_PHONE_STATE)
 
     private lateinit var viewModel: CallLogViewModel
     private lateinit var adapter: CallLogAdapter
@@ -27,13 +32,20 @@ class CallLogActivity : AppCompatActivity() {
         recycler.adapter = adapter
 
         viewModel = Provider.callLogViewModel
+
+        permissionsButton.setOnClickListener { requestPermissions(false) }
+
+        requestPermissions(true)
     }
 
     override fun onResume() {
         super.onResume()
 
-        disposable = viewModel.observeCallLog().subscribe {
-            adapter.setItems(it)
+        if (hasPermissions(this, *permissions)) {
+            observeCallLog()
+            permissionsButton.visibility = View.GONE
+        } else {
+            permissionsButton.visibility = View.VISIBLE
         }
     }
 
@@ -42,24 +54,50 @@ class CallLogActivity : AppCompatActivity() {
         disposable?.dispose()
     }
 
-    private fun checkPermissions() {
-        val permissions = arrayOf(Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_PHONE_STATE)
+    private fun requestPermissions(isStart: Boolean) {
 
-        if (!hasPermissions(this, *permissions)) {
-            ActivityCompat.requestPermissions(this, permissions, PERMISSIONS)
+        if(android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.M) {
+            val denied = permissions.filter {
+                checkSelfPermission(it) == PackageManager.PERMISSION_DENIED
+            }
+
+            val anyPermissionIsPermanentlyDenied = denied.any { !shouldShowRequestPermissionRationale(it) }
+
+            if (anyPermissionIsPermanentlyDenied && !isStart) {
+                // Open the settings so the user can enable those permissions
+                openAppSettings()
+            } else {
+                // Show standard dialog
+                ActivityCompat.requestPermissions(this, permissions, PERMISSIONS)
+            }
+        }
+    }
+
+    private fun observeCallLog() {
+        disposable?.dispose()
+        disposable = viewModel.observeCallLog().subscribe {
+            adapter.setItems(it)
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (requestCode == PERMISSIONS) {
-
+        if (requestCode == PERMISSIONS && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            permissionsButton.visibility = View.GONE
+            observeCallLog()
+        } else {
+            permissionsButton.visibility = View.VISIBLE
         }
-
     }
 
     private fun hasPermissions(context: Context, vararg permissions: String): Boolean = permissions.all {
         ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun openAppSettings() {
+        val intent = Intent(ACTION_APPLICATION_DETAILS_SETTINGS)
+        intent.data = fromParts("package", packageName, null)
+        startActivityForResult(intent, REQUEST_PERMISSION_SETTING)
     }
 }
---
+++ app/src/test/java/net/blakelee/calllog/MockCallLogRepository.kt
@@ -0,0 +1,41 @@
+package net.blakelee.calllog
+
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
+
+class MockCallLogRepository : CallLogInterface {
+
+    override fun observeCallLog(): Observable<List<CallDetails>> {
+        return Observable.create { emitter ->
+            var cur = 0
+            val max = 5
+
+            fun startTimer() {
+                Single.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+                    .subscribe { _ ->
+                        emitter.onNext(generateItems())
+                        if (++cur < max) {
+                            startTimer()
+                        }
+                    }
+            }
+
+            startTimer()
+        }
+    }
+
+    private fun generateItems(): List<CallDetails> {
+        val rand = Random(2)
+        return (0 until 50).map {
+            CallDetails(
+                rand.nextLong(100_000_0000, 999_999_9999).toString(),
+                Date().toString(),
+                listOf("INCOMING", "OUTGOING").random()
+            )
+        }
+    }
+}
